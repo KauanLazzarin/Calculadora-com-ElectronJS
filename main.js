51e2772b2ff0cd7fa72bc9913d7482f340f66293
Kauan Lazzarin <kauanlazzarin@gmail.com>
2020-08-23 22:14:09 -0300

    criando responsividade da primeira funcionalidade

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const { app, BrowserWindow, } = require ('electron')
 
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/node_modules/electron`)
+})
+
 app.whenReady()
     .then(() => {
         const minhaJanela = new BrowserWindow({
-            width: 450,
-            height: 1000,
+            height:720,
+            width:1100,
             webPreferences: {
+                webSecurity: process.env.NODE_ENV !== 'development',
                 nodeIntegration: true
             }
         })
 
         minhaJanela.setMenu(null)
-        minhaJanela.loadFile('index.html')
+        minhaJanela.loadFile(__dirname + '/pages/index.html')
     })
     app.disableHardwareAcceleration()
